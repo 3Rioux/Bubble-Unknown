29c55ffc41de45cebfb25f758820ea04b8256eca
Justin Rioux <122111862+3Rioux@users.noreply.github.com>
2025-01-24 15:53:10 -0400

    Shield orbit around the player using the mouse
    could be improved but good enough for now

--- Assets/_Scripts/Shield/ShieldMovement.cs
+++ Assets/_Scripts/Shield/ShieldMovement.cs
@@ -1,17 +1,64 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
-
-public class ShieldMovement : MonoBehaviour
+namespace _Scripts.Shield
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    /// Script to make the shield move around the player 
+    /// </summary>
+    public class ShieldMovement : MonoBehaviour
     {
-        
-    }
+        public GameObject orb;
+        public float radius;
+        public float radiusSpeed;
+        public float rotationSpeed;
 
-    // Update is called once per frame
-    void Update()
-    {
+        private Transform centre;
+        private Vector3 desiredPos;
+
+        void Start()
+        {
+            centre = orb.transform;
+            transform.position = (transform.position - centre.position).normalized * radius + centre.position;
+        }
+
+        void Update()
+        {
+            float rotationX = Input.GetAxis("Mouse X") * -rotationSpeed;
+            transform.RotateAround(centre.position, Vector3.forward, rotationX);
+
+            desiredPos = (transform.position - centre.position).normalized * radius + centre.position;
+            transform.position = Vector3.MoveTowards(transform.position, desiredPos, radiusSpeed * Time.deltaTime);
+        }
         
-    }
-}
+        
+        /*[SerializeField] private GameObject shield;// holds the shield GameObject or Prefab 
+
+        [SerializeField] private float radius;
+         private  Transform pivot;
+
+        private void Start()
+        {
+            // pivot = shield.transform;
+            // transform.parent = pivot;
+            // transform.position += Vector3.up * radius;
+            
+        }
+
+        void Update()
+        {
+            
+            
+            //Vector3 orbVector = Camera.main.WorldToScreenPoint(shield.position);
+            // orbVector = Input.mousePosition - orbVector;
+            // float angle = Mathf.Atan2(orbVector.y, orbVector.x) * Mathf.Rad2Deg;
+            //
+            // pivot.position = shield.position;
+            // pivot.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+            
+        }
+        */
+   
+        
+    }//end ShieldMovement
+}//end namespace

84e611e64d70a8a838bee62725136c5fe7936928
TherealRioux <justinrioux0@outlook.com>
2025-01-24 20:46:52 -0800

    Added colliders to shield

--- Assets/_Scripts/Shield/ShieldMovement.cs
+++ Assets/_Scripts/Shield/ShieldMovement.cs
@@ -9,32 +9,74 @@ namespace _Scripts.Shield
     /// </summary>
     public class ShieldMovement : MonoBehaviour
     {
-       
-       [SerializeField] private GameObject orb;
-       [SerializeField] private float radius;
-       [SerializeField] private float radiusSpeed;
-       [SerializeField] private float rotationSpeed;
+        private Camera cam;
 
-        private Transform centre;
-        private Vector3 desiredPos;
-
-        void Start()
+        private Vector2 MousePos
         {
-            centre = orb.transform;
-            transform.position = (transform.position - centre.position).normalized * radius + centre.position;
+            get
+            {
+                Vector2 pos = cam.ScreenToWorldPoint(Input.mousePosition);
+                return pos;
+            }
         }
 
-        void Update()
-        {
-            float rotationX = Input.GetAxis("Mouse X") * -rotationSpeed;
-            transform.RotateAround(centre.position, Vector3.forward, rotationX);
 
-            desiredPos = (transform.position - centre.position).normalized * radius + centre.position;
-            transform.position = Vector3.MoveTowards(transform.position, desiredPos, radiusSpeed * Time.deltaTime);
+        private void Awake()
+        {
+            cam = Camera.main;
         }
-        
-       
-   
-        
+
+        [SerializeField] private GameObject orb;
+        [SerializeField] private float radius;
+        [SerializeField] private float radiusSpeed;
+        [SerializeField] private float rotationSpeed;
+
+         private Transform centre;
+         private Vector3 desiredPos;
+
+         void Start()
+         {
+             centre = orb.transform;
+             transform.position = (transform.position - centre.position).normalized * radius + centre.position;
+         }
+
+         void Update()
+         {
+             float rotationX = Input.GetAxis("Mouse X") * -rotationSpeed;
+             transform.RotateAround(centre.position, Vector3.forward, rotationX);
+
+             desiredPos = (transform.position - centre.position).normalized * radius + centre.position;
+             transform.position = Vector3.MoveTowards(transform.position, desiredPos, radiusSpeed * Time.deltaTime);
+         }
+
+        //rotate around pivot of this object 
+        //private Camera cam;
+
+        //private Vector2 MousePos
+        //{
+        //    get
+        //    {
+        //        Vector2 pos = cam.ScreenToWorldPoint(Input.mousePosition);
+        //        return pos;
+        //    }
+        //}
+
+
+        //private void Awake()
+        //{
+        //    cam = Camera.main;
+        //}
+
+
+        //private void Update()
+        //{
+        //    Vector2 dir = (Vector2)transform.position - MousePos;
+
+        //    float angle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg);
+
+        //    transform.eulerAngles = new Vector3(0f,0f,angle);
+
+        //}
+
     }//end ShieldMovement
 }//end namespace
